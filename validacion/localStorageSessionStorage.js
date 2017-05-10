895daaf5226bfaa778339beabc94db3d382429f1
alumno <alumno>
2017-05-09 21:35:32 -0300

    Clase 7 Tercer commit Local storage y Session storage

---
+++ validacion/localStorageSessionStorage.js
@@ -0,0 +1,6 @@
+if(typeof(storage)!=="undefined"){
+    localStorage.setItem("autor","Nombre");//localStorage.autor."Nombre";
+    //var autor = localStorage.getItem("autor"); localStorage.removeItem("autor");
+}else{
+    alert("No está instalado");
+}
